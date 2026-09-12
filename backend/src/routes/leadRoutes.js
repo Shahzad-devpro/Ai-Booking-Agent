941e28eb89e3a0ca9f71dff29ab5478ff2f87bf9
Shahzad-devpro <shahzad.devpro@gmail.com>
2026-09-12 16:47:05 +0500

    JWT middleware authentication done

--- backend/src/routes/leadRoutes.js
+++ backend/src/routes/leadRoutes.js
@@ -2,12 +2,13 @@ const express = require("express");
 const { createLead, getAllLeads, getLeadById, updateLeadStatus } = require("../controllers/leadController");
 const { validLeadStatus, validateCreateLead } = require("../validators/leadValidator");
 const { validateLeadQuery } = require("../validators/queryValidator");
+const authMiddleware = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 
 router.post("/", validateCreateLead, createLead);
-router.get("/", validateLeadQuery, getAllLeads);
+router.get("/", authMiddleware, validateLeadQuery, getAllLeads);
 router.get("/:id",getLeadById);
 router.patch("/:id/status", validLeadStatus, updateLeadStatus);
 
